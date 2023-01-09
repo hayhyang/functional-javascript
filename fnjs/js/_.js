999bb481fe72e182025ab18bc9e00b78e93cb0a9
hay <hayhyang@gmail.com>
2023-01-09 23:16:39 +0900

    2. 함수형으로 전환하기 - go, pipe

--- fnjs/js/_.js
+++ fnjs/js/_.js
@@ -1,19 +1,3 @@
-const slice = Array.prototype.slice;
-function _rest(list, num) {
-  return slice.call(list, num || 1);
-}
-
-function _reduce(list, iter, memo) {
-  if (arguments.length == 2) {
-    memo = list[0];
-    list = _rest(list);
-  }
-  _each(list, function (val) {
-    memo = iter(memo, val);
-  });
-  return memo;
-}
-
 function _curry(fn) {
   return function (a, b) {
     return arguments.length === 2
@@ -62,4 +46,38 @@ function _each(list, iter) {
     iter(list[i]);
   }
   return list;
+}
+
+const slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+
+function _reduce(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, function (val) {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
+
+function _pipe() {
+  const fns = arguments;
+  return function (arg) {
+    return _reduce(
+      fns,
+      function (arg, fn) {
+        return fn(arg);
+      },
+      arg
+    );
+  };
+}
+
+function _fo(arg) {
+  const fns = _rest(arguments);
+  return _pipe.apply(null, fns)(arg);
 }
