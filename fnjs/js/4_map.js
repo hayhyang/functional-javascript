606faa44fd1a29bb5a4b6afbb729cdf5ef5f061a
hay <hayhyang@gmail.com>
2023-01-15 14:50:43 +0900

    map

---
+++ fnjs/js/4_map.js
@@ -0,0 +1,22 @@
+const products = [
+  { name: "tv", price: 1000 },
+  { name: "radio", price: 6000 },
+  { name: "desk", price: 2000 },
+  { name: "paper", price: 3000 },
+  { name: "plant", price: 9000 },
+  { name: "chair", price: 7000 },
+  { name: "bed", price: 10000 },
+];
+
+const map = (f, iter) => {
+  let result = [];
+  for (const a of iter) {
+    result.push(f(a));
+  }
+  return result;
+};
+
+const names = map((item) => item.name, products);
+console.log(names);
+const prices = map((item) => item.price, products);
+console.log(prices);
